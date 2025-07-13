341ca606e23725f5ea57e0724550b6315d4e082f
Adiflickzzz <adivardhan2248@gmail.com>
2025-07-14 01:55:37 +0530

    structured folders

--- hello_world/target/debug/deps/hello_world-d73cedcabe85f7e8.d
+++
@@ -1,5 +0,0 @@
-/home/adi/rust-sprint/hello_world/target/debug/deps/hello_world-d73cedcabe85f7e8: src/main.rs
-
-/home/adi/rust-sprint/hello_world/target/debug/deps/hello_world-d73cedcabe85f7e8.d: src/main.rs
-
-src/main.rs:
